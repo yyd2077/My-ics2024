cbf1460fe0c2b66ff0eefc259d80f789790dc03d
tracer-ics2024 <tracer@njuics.org>
2024-11-04 21:23:31 +0800

    >  compile NEMU 231220000 张三 Linux qiqi-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:31 up  1:41,  1 user,  load average: 0.75, 0.80, 0.61

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -1,16 +1,15 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+* 版权所有 (c) 2014-2024 Zihao Yu, 南京大学
 *
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
+* NEMU遵循Mulan PSL v2.0协议。
+* 您可以根据Mulan PSL v2.0协议的条款和条件使用此软件。
+* 您可以在以下地址获取Mulan PSL v2.0协议的副本：
 *          http://license.coscl.org.cn/MulanPSL2
 *
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+* 此软件按“原样”提供，不附带任何明示或暗示的保证，
+* 包括但不限于非侵权、适销性或适用于特定目的的保证。
 *
-* See the Mulan PSL v2 for more details.
+* 请参阅Mulan PSL v2.0协议以获取更多详细信息。
 ***************************************************************************************/
 
 #include <isa.h>
@@ -24,11 +23,11 @@ void init_device();
 void init_sdb();
 void init_disasm();
 
+// 欢迎信息
 static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
-  IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
-        "to record the trace. This may lead to a large log file. "
-        "If it is not necessary, you can disable it in menuconfig"));
+  IFDEF(CONFIG_TRACE, Log("如果启用了跟踪，将生成一个日志文件来记录跟踪。这可能会导致日志文件非常大。"
+        "如果不需要，可以在menuconfig中禁用它"));
   Log("Build time: %s, %s", __TIME__, __DATE__);
   printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
@@ -39,26 +38,32 @@ static void welcome() {
 #ifndef CONFIG_TARGET_AM
 #include <getopt.h>
 
+// 设置批处理模式
 void sdb_set_batch_mode();
 
+// 日志文件
 static char *log_file = NULL;
+// 差分测试文件
 static char *diff_so_file = NULL;
+// 图像文件
 static char *img_file = NULL;
+// 差分测试端口
 static int difftest_port = 1234;
 
+// 加载图像
 static long load_img() {
   if (img_file == NULL) {
-    Log("No image is given. Use the default build-in image.");
-    return 4096; // built-in image size
+    Log("没有给出图像。使用默认的内置图像。");
+    return 4096; // 内置图像大小
   }
 
   FILE *fp = fopen(img_file, "rb");
-  Assert(fp, "Can not open '%s'", img_file);
+  Assert(fp, "无法打开'%s'", img_file);
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
 
-  Log("The image is %s, size = %ld", img_file, size);
+  Log("图像为%s，大小为%ld", img_file, size);
 
   fseek(fp, 0, SEEK_SET);
   int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
@@ -68,6 +73,7 @@ static long load_img() {
   return size;
 }
 
+// 解析参数
 static int parse_args(int argc, char *argv[]) {
   const struct option table[] = {
     {"batch"    , no_argument      , NULL, 'b'},
@@ -98,42 +104,44 @@ static int parse_args(int argc, char *argv[]) {
   return 0;
 }
 
+// 初始化监视器
 void init_monitor(int argc, char *argv[]) {
-  /* Perform some global initialization. */
+  /* 执行一些全局初始化。 */
 
-  /* Parse arguments. */
+  /* 解析参数。 */
   parse_args(argc, argv);
 
-  /* Set random seed. */
+  /* 设置随机种子。 */
   init_rand();
 
-  /* Open the log file. */
+  /* 打开日志文件。 */
   init_log(log_file);
 
-  /* Initialize memory. */
+  /* 初始化内存。 */
   init_mem();
 
-  /* Initialize devices. */
+  /* 初始化设备。 */
   IFDEF(CONFIG_DEVICE, init_device());
 
-  /* Perform ISA dependent initialization. */
+  /* 执行与ISA相关的初始化。 */
   init_isa();
 
-  /* Load the image to memory. This will overwrite the built-in image. */
+  /* 将图像加载到内存中。这将覆盖内置图像。 */
   long img_size = load_img();
 
-  /* Initialize differential testing. */
+  /* 初始化差分测试。 */
   init_difftest(diff_so_file, img_size, difftest_port);
 
-  /* Initialize the simple debugger. */
+  /* 初始化简单调试器。 */
   init_sdb();
 
   IFDEF(CONFIG_ITRACE, init_disasm());
 
-  /* Display welcome message. */
+  /* 显示欢迎信息。 */
   welcome();
 }
 #else // CONFIG_TARGET_AM
+// 加载图像
 static long load_img() {
   extern char bin_start, bin_end;
   size_t size = &bin_end - &bin_start;
@@ -142,6 +150,7 @@ static long load_img() {
   return size;
 }
 
+// 初始化监视器
 void am_init_monitor() {
   init_rand();
   init_mem();
--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -1,35 +1,42 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+* 版权所有 (c) 2014-2024 周子豪，南京大学
 *
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
+* NEMU遵循Mulan PSL v2.0协议。
+* 您可以根据Mulan PSL v2.0协议的条款和条件使用此软件。
+* 您可以在以下链接获取Mulan PSL v2.0协议的副本：
 *          http://license.coscl.org.cn/MulanPSL2
 *
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+* 此软件按“原样”提供，不附带任何形式的明示或暗示保证，
+* 包括但不限于非侵权、适销性或适用于特定目的的保证。
 *
-* See the Mulan PSL v2 for more details.
+* 有关Mulan PSL v2.0协议的详细信息，请参阅Mulan PSL v2.0协议。
 ***************************************************************************************/
 
 #include <common.h>
 
+// 初始化监视器
 void init_monitor(int, char *[]);
+// 初始化AM监视器
 void am_init_monitor();
+// 启动引擎
 void engine_start();
+// 检查退出状态是否良好
 int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {
-  /* Initialize the monitor. */
+  /* 初始化监视器。 */
 #ifdef CONFIG_TARGET_AM
+  // 如果定义了CONFIG_TARGET_AM，则调用am_init_monitor()函数
   am_init_monitor();
 #else
+  // 否则，调用init_monitor()函数
   init_monitor(argc, argv);
 #endif
 
-  /* Start engine. */
+  /* 启动引擎。 */
+  // 调用engine_start()函数
   engine_start();
 
+  // 返回退出状态是否良好
   return is_exit_status_bad();
 }
