7894b1846ccef8ff6bd6b31b2f6493d45e1805fe
tracer-ics2024 <tracer@njuics.org>
2024-11-05 20:06:46 +0800

    >  compile NEMU 231220000 张三 Linux qiqi-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:06:46 up 8 min,  1 user,  load average: 0.10, 0.70, 0.49

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -1,15 +1,15 @@
 /***************************************************************************************
-* 版权所有 (c) 2014-2024 周子豪，南京大学
+* 版权所有 (c) 2014-2024 Zihao Yu, 南京大学
 *
-* NEMU遵循Mulan PSL v2.0协议。
-* 您可以根据Mulan PSL v2.0协议的条款和条件使用此软件。
-* 您可以在以下链接获取Mulan PSL v2.0协议的副本：
+* NEMU遵循Mulan PSL v2.0开源许可协议。
+* 您可以根据Mulan PSL v2.0的条款和条件使用此软件。
+* 您可以在以下链接获取Mulan PSL v2.0的副本：
 *          http://license.coscl.org.cn/MulanPSL2
 *
-* 此软件按“原样”提供，不附带任何形式的明示或暗示保证，
-* 包括但不限于非侵权、适销性或适用于特定目的的保证。
+* 此软件按“原样”提供，不附带任何明示或暗示的保证，
+* 包括但不限于非侵权、适销性或特定用途适用性。
 *
-* 有关Mulan PSL v2.0协议的详细信息，请参阅Mulan PSL v2.0协议。
+* 有关详细信息，请参阅Mulan PSL v2.0。
 ***************************************************************************************/
 
 #include <common.h>
@@ -24,17 +24,16 @@ void engine_start();
 int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {
-  /* 初始化监视器。 */
+  // 初始化监视器
 #ifdef CONFIG_TARGET_AM
-  // 如果定义了CONFIG_TARGET_AM，则调用am_init_monitor()函数
+  // 如果定义了CONFIG_TARGET_AM，则初始化AM监视器
   am_init_monitor();
 #else
-  // 否则，调用init_monitor()函数
+  // 否则，初始化普通监视器
   init_monitor(argc, argv);
 #endif
 
-  /* 启动引擎。 */
-  // 调用engine_start()函数
+  // 启动引擎
   engine_start();
 
   // 返回退出状态是否良好
